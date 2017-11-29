17dbf6b027ca31793c03fc52770f0e919e716ad5
Roshan Sawant <roshansawant70@yahoo.com>
2017-11-29 23:48:52 +0530

    added script to upload files from sftp to jira

---
+++ upload_jira.py
@@ -0,0 +1,25 @@
+import pysftp,glob
+from jira import JIRA
+
+srv = pysftp.Connection(host="", username="",password="")
+authed_jira = JIRA('https://vuclipdev.atlassian.net/', basic_auth=(username, passowrd))
+
+issue=raw_input('Enter jira ID::')
+files=srv.listdir('/tmp/test1/')
+file=srv.listdir('/tmp/test2/')
+
+for i,j in zip(files,file):
+	srv.chdir('/tmp/test1/')
+	print i
+	print srv.get(i)
+	srv.chdir('/tmp/test2/')
+	print j
+	print srv.get(j)
+
+for opfiles in glob.glob('/home/roshans/testing/v*.csv'):
+	authed_jira.add_attachment(issue,opfiles)
+
+authed_jira.add_comment(issue,'files uploaded')
+
+
+#print srv.get('/tmp/vodafone_modify_reconcile_test/outputstep1act/*', '/home/roshans/testing/' )
